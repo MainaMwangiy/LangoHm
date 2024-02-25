5fa4f673bf4059cb5b2f83bd3b92bb44d348e00d
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-02-25 20:46:01 +0300

    add vehicle details for selected user

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -1,5 +1,4 @@
 const { executeQuery } = require("../models");
-const Vehicle = require("../models/vehicles");
 
 exports.ListVehicle = async (req, res, next) => {
     try {
@@ -22,3 +21,24 @@ exports.ListVehicle = async (req, res, next) => {
     }
 };
 
+exports.getUserVehicleDetails = async (req, res, next) => {
+    const { id } = { ...req.params, ...req.body, ...req.query }
+    try {
+        const sql = `SELECT * FROM vehicles WHERE user_id = '${id}'`;
+        const vehicle = await executeQuery(sql)
+        if (vehicle) {
+            res.status(200).json({
+                success: true,
+                message: "Vehicle Loaded Successfully",
+                data: vehicle
+            })
+        }
+    } catch (error) {
+        if (error) {
+            return res.json({
+                success: false,
+                errors: "No details found",
+            });
+        }
+    }
+}
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -3,5 +3,6 @@ const controllers = require("../controllers");
 const router = require("express").Router()
 
 router.post("/list", controllers.ListVehicle)
+router.post("/list/:id", controllers.getUserVehicleDetails)
 
 module.exports = router;
