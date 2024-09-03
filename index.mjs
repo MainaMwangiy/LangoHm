244d0de82df5ee26cdc024edb29f53dce80b983e
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-09-03 23:16:56 +0300

    #41 - order notifications using recent ones

--- index.mjs
+++ index.mjs
@@ -22,44 +22,39 @@ app.get("/", (req, res) => {
 app.use("/auth", routes.auth);
 app.use("/vehicles", routes.vehicles);
 
-// Send notification
-app.all('/api/notifications', async (req, res) => {
-    const { senderId, receiverId, content, notificationType } = { ...req.body, ...req.query, ...req.params };
+app.all('/api/notifications/create', async (req, res) => {
+    const { user_id, content, notification_type, status } = { ...req.body, ...req.query, ...req.params };
     const result = await executeQuery(
-        'INSERT INTO notifications (senderId, receiverId, content, notificationType) VALUES ($1, $2, $3, $4) RETURNING *',
-        [senderId, receiverId, content, notificationType]
+        'INSERT INTO notifications (user_id, content, notification_type,status) VALUES ($1, $2, $3, $4) RETURNING *',
+        [user_id, content, notification_type, status]
     );
     res.status(200).json({ success: true, data: result });
 });
 
-// Get all notification - for admin
 app.all('/api/notifications/all', async (req, res) => {
-    const sql = 'SELECT * FROM notifications';
+    const sql = 'SELECT * FROM notifications ORDER BY created_at DESC';
     const results = await executeQuery(sql);
     res.status(200).json({ success: true, data: results });
 });
 
-// Fetch notifications for admin
 app.all('/api/notifications/admin', async (req, res) => {
     const { id } = { ...req.body, ...req.query, ...req.params };
-    const sql = 'SELECT * FROM notifications WHERE receiverId = $1';
+    const sql = 'SELECT * FROM notifications WHERE user_id = $1';
     const results = await executeQuery(sql, [id]);
     res.status(200).json({ success: true, data: results });
 });
 
-// Fetch notifications for a normal user
 app.all('/api/notifications/user', async (req, res) => {
     const { id } = { ...req.body, ...req.query, ...req.params };
-    const sql = 'SELECT * FROM notifications WHERE receiverId = $1';
+    const sql = 'SELECT * FROM notifications WHERE user_id = $1';
     const results = await executeQuery(sql, [id]);
     res.status(200).json({ success: true, data: results });
 });
 
-// Mark as read
 app.all('/api/notifications/read/:id', async (req, res) => {
     const { id } = req.params;
     const results = await executeQuery(
-        'UPDATE notifications SET status = \'read\' WHERE notificationId = $1 RETURNING *',
+        'UPDATE notifications SET status = \'read\' WHERE notification_id = $1 RETURNING *',
         [id]
     );
     res.status(200).json({ success: true, data: results });
